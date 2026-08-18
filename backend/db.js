d51aff4947a95fbe742c08bb59031723266a2137
Selene <karen_08_121@hotmail.com>
2026-08-18 16:00:44 -0500

    habilitar conexion PostgreSQL en produccion

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,17 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const config = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+    }
+  : {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    };
+
+const pool = new Pool(config);
 
 module.exports = pool;
